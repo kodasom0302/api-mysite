4c1caf64eb61f1bfd3bf6bc35c6c87e0b31b13a4
kodasom0302 <dasom589@gmail.com>
2024-04-29 10:22:47 +0900

    2024-04-29

---
+++ src/main/java/com/javaex/service/UserService.java
@@ -0,0 +1,48 @@
+package com.javaex.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.UserDao;
+import com.javaex.vo.UserVo;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	//회원가입
+	public int exeJoin(UserVo userVo) {
+		System.out.println("UserService.exeJoin()");
+		
+		int count=userDao.userInsert(userVo);
+		
+		return count;
+	}
+	
+	// 회원정보 수정
+	public int exeModify(UserVo userVo) {
+		System.out.println("UserService.exeModify()");
+
+		int count = userDao.userUpdate(userVo);
+		return count;
+	}
+	
+	//회원정보 수정폼(1명 데이터 가져오기)
+	public UserVo exeModifyForm(int no) {
+		System.out.println("UserService.exeModifyForm()");
+
+		UserVo userVo = userDao.userSelectOneByNo(no);
+		return userVo;
+	}
+	
+	//로그인
+	public UserVo exeLogin(UserVo userVo) {
+		System.out.println("UserService.exeLogin()");
+		
+		UserVo authUser = userDao.userSelectByIdPw(userVo);
+		return authUser;
+	}
+	
+}
